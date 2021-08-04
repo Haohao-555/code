aa61f6d079d11d7fad0ea318d64adf41c24ff5d1
Haohao-555 <3283627490@qq.com>
2021-08-04 19:08:26 +0800

    8.4

---
+++ javascript/day08/data.js
@@ -0,0 +1,470 @@
+var data = [
+    {
+        name: '老夫子',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/5bc575290295759acc6613609d638c97.jpg',
+    },
+    {
+        name: '钟无艳',
+        position: '战士,坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/c830fa0c2a9836e4879f5dcd955774a4.jpg',
+    },
+    {
+        name: '典韦',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/e12c1f19ac0b9b1d1fb97171a11cb0e8.jpg',
+    },
+    {
+        name: '韩信',
+        position: '刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/4a738b81cf93cb4d55d00a627b50d0a5.jpg',
+    },
+    {
+        name: '项羽',
+        position: '坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/35783bfaced7570d632272c7af1946f6.jpg',
+    },
+    {
+        name: '赵云',
+        position: '战士,刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/407f339a2d86615af3351f4009572ed3.jpg',
+    },
+    {
+        name: '亚瑟',
+        position: '战士,坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/b0b1171831a055c9fdc230aec1d8074f.jpg',
+    },
+    {
+        name: '曹操',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/5a2864e75248990600147e5dbce5c180.jpg',
+    },
+    {
+        name: '孙悟空',
+        position: '刺客,战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/7fba80e36114d94c320bfe284853e3c1.jpg',
+    },
+    {
+        name: '吕布',
+        position: '战士,坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/b3b0c1b932ad8c54f7c152bd572db889.jpg',
+    },
+    {
+        name: '关羽',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/bb52a26bc5ca84785bf26e32791baae8.jpg',
+    },
+    {
+        name: '夏侯惇',
+        position: '坦克,战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/4927297c771274a6743ac7f5bcdf4eb4.jpg',
+    },
+    {
+        name: '杨戬',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/54904e2471987e47e5d7deabca79cdd5.jpg',
+    },
+    {
+        name: '雅典娜',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/921ce9451bed324922be096731f5f1af.jpg',
+    },
+    {
+        name: '橘右京',
+        position: '刺客,战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/da13121da55770a1758c7351b93d160c.jpg',
+    },
+    {
+        name: '哪吒',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/540244fdfe46a605995f955489c64865.jpg',
+    },
+    {
+        name: '姜子牙',
+        position: '法师,辅助',
+        img: 'http://img.pipaw.com/wy/zq/63780/4d211b720018d277172179fc45b70882.jpg',
+    },
+    {
+        name: '武则天',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/fe771cb59eed3876c586406ddfbd15d3.jpg',
+    },
+    {
+        name: '高渐离',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/83aabbfcd820581d49d0252d2f03c61a.jpg',
+    },
+    {
+        name: '王昭君',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/d30dd0e1c7b85790181fc0110c64fa88.jpg',
+    },
+    {
+        name: '小乔',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/3113951cc1fcb554310eca94fba033e4.jpg',
+    },
+    {
+        name: '扁鹊',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/ac4eb8bf30ba51bb32790975b7616a76.png',
+    },
+    {
+        name: '妲己',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/e4b1232c8083df60259c864b53a1813f.jpg',
+    },
+    {
+        name: '嬴政',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/6c65f21c057adc0732341494246adc90.jpg',
+    },
+    {
+        name: '安琪拉',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/84e9f1a171846bec85eb6fefedc19fdb.jpg',
+    },
+    {
+        name: '张良',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/021bb5127d0da5325e2b1f1af34c3624.jpg',
+    },
+    {
+        name: '周瑜',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/7799e8c92d7e2b165e660db7164ab7a4.jpg',
+    },
+    {
+        name: '芈月',
+        position: '法师,坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/b05fad8af2406e0b8983493328d4658a.jpg',
+    },
+    {
+        name: '貂蝉',
+        position: '法师,刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/4f6f5e62ace8afcd88a7f0c15aadcb1c.jpg',
+    },
+    {
+        name: '不知火舞',
+        position: '法师,刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/588db0bf3b1259ae5e031f0c70a4fbf0.jpg',
+    },
+    {
+        name: '诸葛亮',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/14a79c0d682e80d9a17ddea0ba852f7a.jpg',
+    },
+    {
+        name: '东皇太一',
+        position: '坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/1e66ef7eda476965e09ad0ee85b78c7a.jpg',
+    },
+    {
+        name: '干将莫邪',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/136ca0d5c3dfd04bb472eda4645f94bc.jpg',
+    },
+    {
+        name: '达摩',
+        position: '战士,坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/bce4df948927d14bdecbcbb552b23a53.jpg',
+    },
+    {
+        name: '廉颇',
+        position: '坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/5800f266c889fd29f3f4cbdb9d9f44fa.jpg',
+    },
+    {
+        name: '庄周',
+        position: '辅助,坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/a6df71c618958848f8d97514f96d88ca.jpg',
+    },
+    {
+        name: '刘禅',
+        position: '辅助,坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/8e6fecbcf2b27ef2acf5696953e1566a.jpg',
+    },
+    {
+        name: '陈咬金',
+        position: '坦克,战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/3483ca2b99326f33049a88c6c261e56d.jpg',
+    },
+    {
+        name: '墨子',
+        position: '法师,战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/74817095b631748b9dc435663e0865ba.jpg',
+    },
+    {
+        name: '白起',
+        position: '坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/82dc8868ee89a2b5bcfad08b4ef20790.jpg',
+    },
+    {
+        name: '钟馗',
+        position: '法师,战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/282c1248ec1d90cefe0f62eeb39e0037.jpg',
+    },
+    {
+        name: '张飞',
+        position: '辅助',
+        img: 'http://img.pipaw.com/wy/zq/63780/c891d363a462a7f3cdb19a630c5b64ee.jpg',
+    },
+    {
+        name: '刘邦',
+        position: '坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/f7102142ff4fca5614db3b5c7b5d90a9.jpg',
+    },
+    {
+        name: '娜可露露',
+        position: '刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/f2aae2c3f30540b45e3347f6498be461.jpg',
+    },
+    {
+        name: '露娜',
+        position: '战士,法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/395c1ac73c2b1ab5788868d8141f2f57.jpg',
+    },
+    {
+        name: '阿珂',
+        position: '刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/530bc2599a5bb105912a3dd27292fef9.jpg',
+    },
+    {
+        name: '宫本武藏',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/77a2a09a2ec0b714881867f15b5e31cd.jpg',
+    },
+    {
+        name: '李白',
+        position: '刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/462a7901935d3dc44fa03c7a6cf52cd4.jpg',
+    },
+    {
+        name: '花木兰',
+        position: '战士,刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/c496e4138f0c35da5c16cbabf43db923.jpg',
+    },
+    {
+        name: '兰陵王',
+        position: '刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/72aec360eaf323388e73031cefdc5b8e.jpg',
+    },
+    {
+        name: '鲁班七号',
+        position: '射手',
+        img: 'http://img.pipaw.com/wy/zq/63780/5063cc350b11ede1bc4f2b42d6ed4509.jpg',
+    },
+    {
+        name: '孙尚香',
+        position: '射手',
+        img: 'http://img.pipaw.com/wy/zq/63780/b6b624d779038a64681c6dc95097c963.jpg',
+    },
+    {
+        name: '狄仁杰',
+        position: '射手',
+        img: 'http://img.pipaw.com/wy/zq/63780/6a058fe0b35642d94e2a2d1cef40bcc5.jpg',
+    },
+    {
+        name: '后裔',
+        position: '射手',
+        img: 'http://img.pipaw.com/wy/zq/63780/3c8ee6d7d08fde1d6596b68194e36165.jpg',
+    },
+    {
+        name: '刘备',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/9b2b5cbd6a577d94d70dc4457f949f58.jpg',
+    },
+    {
+        name: '李元芳',
+        position: '射手',
+        img: 'http://img.pipaw.com/wy/zq/63780/ded2a3c56c223fe1acb52f3e9cd72ba4.jpg',
+    },
+    {
+        name: '虞姬',
+        position: '射手',
+        img: 'http://img.pipaw.com/wy/zq/63780/75d3786d03609c8bff3237cd6af20b64.jpg',
+    },
+    {
+        name: '马可波罗',
+        position: '射手',
+        img: 'http://img.pipaw.com/wy/zq/63780/ed8a2c4d3523ef8657dd00dfd69f92b7.jpg',
+    },
+    {
+        name: '成吉思汗',
+        position: '射手',
+        img: 'http://img.pipaw.com/wy/zq/63780/0579a29f0b9993eac3ff282ebf356ec7.jpg',
+    },
+    {
+        name: '黄忠',
+        position: '射手',
+        img: 'http://img.pipaw.com/wy/zq/63780/94836fd126998270dc2af8003258a237.jpg',
+    },
+    {
+        name: '孙膑',
+        position: '辅助,法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/85e374785eed2699b6d6cbef69f74171.jpg',
+    },
+    {
+        name: '太乙真人',
+        position: '辅助,坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/757b66bd53068b1f5bc022d4a2fc92f6.jpg',
+    },
+    {
+        name: '蔡文姬',
+        position: '辅助',
+        img: 'http://img.pipaw.com/wy/zq/63780/33982e6c0c4baac44826af44539d2656.jpg',
+    },
+    {
+        name: '大乔',
+        position: '辅助',
+        img: 'http://img.pipaw.com/wy/zq/63780/efa7ff858e6a41154038f50f7e6795a7.jpg',
+    },
+    {
+        name: '鬼谷子',
+        position: '辅助',
+        img: 'http://img.pipaw.com/wy/zq/63780/e592e89f0468645ace30b3ea8e5e21ab.jpg',
+    },
+    {
+        name: '凯',
+        position: '战士,坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/a988f945862d9513769d4c986226f1a3.jpg',
+    },
+    {
+        name: '甄姬',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/f5f00332e3aa21b93413f8a1ad6b0f75.jpg',
+    },
+    {
+        name: '百里守约',
+        position: '射手,刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/45c9cc66c913c03ec1d963014943048d.jpg',
+    },
+    {
+        name: '梦琪',
+        position: '坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/e47e29bfb2132e91d7eead9dd14fe128.jpg',
+    },
+    {
+        name: '苏烈',
+        position: '坦克,战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/53d41c464566667637f1e2de388cb1fb.jpg',
+    },
+    {
+        name: '百里玄策',
+        position: '刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/3fab0d7a979d8b7f881676cc8597610b.jpg',
+    },
+    {
+        name: '女娲',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/d211238eab7b3b1a3568f50c1e7408f6.jpg',
+    },
+    {
+        name: '明世隐',
+        position: '辅助',
+        img: 'http://img.pipaw.com/wy/zq/63780/024496485bff76cc796dd0cd29e570a5.jpg',
+    },
+    {
+        name: '弈星',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/79b0ff2a353d38c4479a1a6f8e183c01.jpg',
+    },
+    {
+        name: '公孙离',
+        position: '射手',
+        img: 'http://img.pipaw.com/wy/zq/63780/e78eb608a9305d178d2620c32b029874.jpg',
+    },
+    {
+        name: '杨玉环',
+        position: '法师,辅助',
+        img: 'http://img.pipaw.com/wy/zq/63780/48154e1ae9c644e6276f02aac5b43f20.jpg',
+    },
+    {
+        name: '裴擒虎',
+        position: '刺客,战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/f8ab51e4f834c79dcc7eab18879c3856.jpg',
+    },
+    {
+        name: '狂铁',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/9e07c1c5939e61f5bb4faab13eccc01d.jpg',
+    },
+    {
+        name: '米莱狄',
+        position: '法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/e5779793e226c629d340d24405d1af69.jpg',
+    },
+    {
+        name: '元歌',
+        position: '刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/6f2b5c32f37cacfe6d6997fc8694fc35.png',
+    },
+    {
+        name: '孙策',
+        position: '坦克,战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/e3c6ccfd3aae60729e525bcaec5c3a7b.jpg',
+    },
+    {
+        name: '司马懿',
+        position: '刺客,法师',
+        img: 'http://img.pipaw.com/wy/zq/63780/0ecdb1ec6f9870947ddc9bf49891002c.png',
+    },
+    {
+        name: '盾山',
+        position: '辅助',
+        img: 'http://img.pipaw.com/wy/zq/63780/359b4ab5ceeb571d4535e825c0bcf8d0.png',
+    },
+    {
+        name: '伽罗',
+        position: '射手',
+        img: 'http://img.pipaw.com/wy/zq/63780/525cd6d1ff12a6a6b021cfeabbdfc450.jpg',
+    },
+    {
+        name: '李信',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/ca65c6dba2982b11588b738571597ee2.jpg',
+    },
+    {
+        name: '上官婉儿',
+        position: '法师,刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/1aa6e742ae3775562b371f9debb7c0e7.jpg',
+    },
+    {
+        name: '嫦娥',
+        position: '法师,坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/53cddbba02a6c47c610894b0990f8440.jpg',
+    },
+    {
+        name: '猪八戒',
+        position: '坦克',
+        img: 'http://img.pipaw.com/wy/zq/63780/2436d581ce297677d54bf71598372b8f.jpg',
+    },
+    {
+        name: '盘古',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/d120d61dcdb96b6be703ae0ebb3bd85f.jpg',
+    },
+    {
+        name: '瑶',
+        position: '辅助',
+        img: 'http://img.pipaw.com/wy/zq/63780/16a088a4eb30d50bd1ca69a36bbcdf54.jpg',
+    },
+    {
+        name: '云中君',
+        position: '刺客,战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/1671d88c3f922a861ff203e974b427c4.jpg',
+    },
+    {
+        name: '曜',
+        position: '战士',
+        img: 'http://img.pipaw.com/wy/zq/63780/9c17ee89a9d3d1ec610bf4bd1348be37.jpg',
+    },
+    {
+        name: '马超',
+        position: '战士,刺客',
+        img: 'http://img.pipaw.com/wy/zq/63780/c8c8a263542e63764f6e75643d315f8d.png',
+    }
+
+]
+
+
