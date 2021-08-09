c96715caeef9a78f8e39dcbb3234d7c7d07ac296
Haohao-555 <3283627490@qq.com>
2021-08-09 18:53:53 +0800

    8.9

---
+++ javascript/day11/作业/data.js
@@ -0,0 +1,108 @@
+//商品数据结构, 按类型分组
+var data = [
+  {
+    type: 'jiadian',
+    id: 'A001',
+    smallImg: '商品小图片',
+    title: '最新家电',
+    largeImg: '商品大图片',
+    price: 178,
+    isPlus: true,
+    plusPrice: 120,
+    isDiscount: false,
+    isJD: true,
+    name: '电饭锅'
+  },
+  {
+    type: 'shengxian',
+    id: 'B912',
+    smallImg: '商品小图片',
+    title: '新鲜花生',
+    largeImg: '商品大图片',
+    price: 28,
+    isPlus: false,
+    plusPrice: 0,
+    isDiscount: true,
+    isJD: false,
+    name: '广西花生'
+  },
+  {
+    type: 'shengxian',
+    id: 'B610',
+    smallImg: '商品小图片',
+    title: '大颗葵米',
+    largeImg: '商品大图片',
+    price: 4,
+    isPlus: false,
+    plusPrice: 0,
+    isDiscount: true,
+    isJD: true,
+    name: '葵米'
+  },
+  {
+    type: 'dianzi',
+    id: 'CF01',
+    smallImg: '商品小图片',
+    title: '最新款华为5G手机',
+    largeImg: '商品大图片',
+    price: 18888,
+    isPlus: false,
+    plusPrice: 0,
+    isDiscount: true,
+    isJD: true,
+    name: '华为5G手机'
+  },
+  {
+    type: 'jiadian',
+    id: 'PU09',
+    smallImg: '商品小图片',
+    title: '高性能电磁炉',
+    largeImg: '商品大图片',
+    price: 456,
+    isPlus: false,
+    plusPrice: 0,
+    isDiscount: true,
+    isJD: true,
+    name: '电磁炉'
+  },
+  {
+    type: 'fuzhuang',
+    id: 'OM10',
+    smallImg: '商品小图片',
+    title: '上新T恤',
+    largeImg: '商品大图片',
+    price: 139,
+    isPlus: false,
+    plusPrice: 0,
+    isDiscount: true,
+    isJD: true,
+    name: 'T恤'
+  },
+  {
+    type: 'jiadian',
+    id: 'KKM01',
+    smallImg: '商品小图片',
+    title: '热水壶',
+    largeImg: '商品大图片',
+    price: 68,
+    isPlus: false,
+    plusPrice: 0,
+    isDiscount: true,
+    isJD: true,
+    name: '静音热水壶'
+  },
+   {
+    type: 'shengxian',
+    id: 'UUY01',
+    smallImg: '商品小图片',
+    title: '鲜虾',
+    largeImg: '商品大图片',
+    price: 398,
+    isPlus: false,
+    plusPrice: 0,
+    isDiscount: true,
+    isJD: true,
+    name: '九节虾'
+  }
+
+];
