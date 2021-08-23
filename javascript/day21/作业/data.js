9b319fecfdbd120a7e58f4165572d934a3e8ba66
Haohao-555 <3283627490@qq.com>
2021-08-23 18:40:40 +0800

    8.23

---
+++ javascript/day21/作业/data.js
@@ -0,0 +1,64 @@
+var data = [
+    { pet: "宝宝金" },
+    { pet: "二哈大G" },
+    { pet: "蛋宝" },
+    { pet: "金渐层" },
+    { pet: "金爱罗夫妇" },
+    { pet: "阿拉丁狗蛋" },
+    { pet: "阿拉" },
+    { pet: "阿拉大大" },
+    { pet: "二哈" },
+    { pet: "柯基" },
+    { pet: "柯基犬" }
+]
+
+var comments = [
+    {
+        username: "用户1",
+        tel: "13180990862",
+        pic: "https://img.meituan.net/avatar/d6ff6e5db3b592f27dc54565cf01397d33354.jpg%40120w_120h_0e_1l%7Cwatermark%3D0",
+        score: 4.4,
+        comment: "这家饭店很好吃，菜品价格亲民。喜欢吃辣的小伙伴值得一试",
+        date: 1624827696404,
+    },
+    {
+        username: "张力",
+        tel: "15280990864",
+        pic: "https://img.meituan.net/avatar/d6ff6e5db3b592f27dc54565cf01397d33354.jpg%40120w_120h_0e_1l%7Cwatermark%3D0",
+        score: 4.8,
+        comment: "同事们对这家店一致好评！已经是部门聚餐指定地点了",
+        date: 1625827696404,
+    },
+    {
+        username: "某饿了用户",
+        tel: "15380990862",
+        pic: "https://img.meituan.net/avatar/d6ff6e5db3b592f27dc54565cf01397d33354.jpg%40120w_120h_0e_1l%7Cwatermark%3D0",
+        score: 1.4,
+        comment: "可能我吃不了辣，对于我来说太辣了",
+        date: 1626827696404,
+    },
+    {
+        username: "某团用户",
+        tel: "17480990982",
+        pic: "https://img.meituan.net/avatar/d6ff6e5db3b592f27dc54565cf01397d33354.jpg%40120w_120h_0e_1l%7Cwatermark%3D0",
+        score: 3.2,
+        comment: "一般般吧",
+        date: 1627827696404,
+    },
+    {
+        username: "某团用户2",
+        tel: "18480940865",
+        pic: "https://img.meituan.net/avatar/d6ff6e5db3b592f27dc54565cf01397d33354.jpg%40120w_120h_0e_1l%7Cwatermark%3D0",
+        score: 2.8,
+        comment: "服务态度太差了",
+        date: 1628827696404,
+    },
+    {
+        username: "用户张小兵",
+        tel: "15880990878",
+        pic: "https://img.meituan.net/avatar/d6ff6e5db3b592f27dc54565cf01397d33354.jpg%40120w_120h_0e_1l%7Cwatermark%3D0",
+        score: 4.5,
+        comment: "那个经理好漂亮！！！",
+        date: 1639827696404,
+    },
+]
