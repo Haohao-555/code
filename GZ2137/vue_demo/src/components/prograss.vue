30b955e589f93736e1ee8fd5d5f313d5187c1e02
Haohao-555 <3283627490@qq.com>
2021-10-25 22:43:34 +0800

    10.25

--- GZ2137/vue_demo/src/components/prograss.vue
+++ GZ2137/vue_demo/src/components/prograss.vue
@@ -5,23 +5,27 @@
  * @FilePath: \vue_demo\src\components\prograss.vue
 -->
 <template>
-    <div id="prograss">
-        <div class="left">
-            <div class="iconfont item icon-shangyishouge"></div>
-            <div :class="['item','iconfont',play? 'icon-zanting': 'icon-bofang']" @click="play=!play"></div>
-            <div class="iconfont item icon-kuaijin"></div>
-        </div>
-        <div class="right">
-            <div class="prograss-container">
-                <div class="current-time"></div>
-                <div
-                    class="bar"
-                    @mousedown="down=true"
-                ></div>
-        </div>
-        <div class="time">{{dt}}</div>
-    </div>
-    </div>
+  <div id="prograss">
+    <div class="left">
+      <div class="iconfont item icon-shangyishouge"></div>
+      <div
+        :class="['item','iconfont',play? 'icon-zanting': 'icon-bofang']"
+        @click="play=!play"
+      ></div>
+    <div class="iconfont item icon-kuaijin"></div>
+  </div>
+  <div class="right">
+    <div class="prograss-container">
+      <div class="current-time"></div>
+      <div
+        class="bar"
+        @mousedown="down=true"
+      ></div>
+  </div>
+  <!-- <div class="time">{{Math.floor(dt/60)+":"+dt%60}}</div> -->
+  </div>
+  <audio :src="audioUrl" @timeupdate="timeupdate"></audio>
+  </div>
 </template>
 <script>
 export default {
@@ -29,35 +33,52 @@ export default {
   props: {
     // 歌曲 id
     songId: Number,
-   
   },
   data() {
     return {
       // 时长
-      dt: 200, // 01:40
+      dt: 221000, // 01:40
       // 是否按下
       down: false,
       // 音频 url
-      audioUrl: "http://m8.music.126.net/20211025201850/d64945df0bcbec676021762122cba3ab/ymusic/ec15/654d/978d/18e47baaf81561f18f2d6e2e966c8bf8.mp3",
+      audioUrl:
+        "http://m8.music.126.net/20211025224333/9586c31030d2b425c715dcfbeba80c26/ymusic/045d/030c/0559/37bcfed3ec4ff3fd493223d9c99c3aa8.mp3",
       // 进度条长度
       prograssDistance: 0,
       // 当前进度条滑动的距离
       currentDistance: 0,
-       // 是否播放
-      play: true,
+      // 是否播放
+      play: false,
+     
+      timeStamp: 0,
+      
     };
   },
-
+  watch: {
+    play: function() {
+      if (this.play) {
+        document.querySelector("audio").play();
+      } else {
+        document.querySelector("audio").pause();
+      }
+    },
+    timeStamp: function() {
+        let x = this.timeStamp * (this.prograssDistance / this.dt);
+        console.log(x);
+        document.querySelector(".bar").style.left = x + "px";
+        document.querySelector(".current-time").style.width = x + "px";
+    }, 
+  },
   mounted() {
     this.init();
   },
   methods: {
     init() {
-        // 保存进度条长度
+      // 保存进度条长度
       this.prograssDistance =
         document.querySelector(".prograss-container").offsetWidth -
         document.querySelector(".bar").offsetWidth;
-        
+
       document.onmousemove = e => {
         let bar = document.querySelector(".bar");
         let prograss = document.querySelector(".prograss-container");
@@ -73,7 +94,10 @@ export default {
 
           // 判断是否超出范围
           distance = distance < 0 ? 0 : distance;
-          distance = distance > prograss_width - bar_width ? prograss_width - bar_width : distance;
+          distance =
+            distance > prograss_width - bar_width
+              ? prograss_width - bar_width
+              : distance;
           bar.style.left = distance + "px";
           current.style.width = distance + "px";
           // 保存当前滑动的距离
@@ -85,10 +109,17 @@ export default {
         this.currentTime();
       };
     },
-    
+
     currentTime() {
-      let x = Math.floor(this.currentDistance *(this.dt / this.prograssDistance));
+      let x = Math.floor(
+        this.currentDistance * (this.dt / this.prograssDistance)
+      );
       console.log(x);
+    },
+
+    timeupdate(e) {
+      console.log(e);
+      this.timeStamp = e.timeStamp;
     }
   },
 };
@@ -117,7 +148,7 @@ export default {
       background-size: 40px 40px;
       background-position: 10px 10px;
       &:hover {
-          color: #31c27c;
+        color: #31c27c;
       }
       &:last-child {
         margin-right: 0;
@@ -126,7 +157,7 @@ export default {
         margin-left: -30px;
       }
       &:nth-child(2) {
-          font-size: 40px;
+        font-size: 40px;
       }
       &:nth-child(1) {
         padding-top: 5px;
@@ -134,7 +165,6 @@ export default {
         padding-left: 40px;
       }
     }
-   
   }
   .right {
     display: flex;
@@ -165,7 +195,7 @@ export default {
       }
     }
     .time {
-      color: #ffffff;
+      color: red;
       margin-left: 12px;
     }
   }
