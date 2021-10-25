bba30fe25b8749c9f2a405ad972976b637648c71
Haohao-555 <3283627490@qq.com>
2021-10-25 16:17:35 +0800

    10.25

---
+++ GZ2137/vue_demo/src/App.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Date: 2021-10-15 15:24:51
+ * @Author: 浩
+ * @LastEditors: 浩
+ * @FilePath: \vue_demo\src\App.vue
+-->
+<template>
+  <div id="app">
+     123413413515253
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+   
+  }
+}
+</script>
+
--- GZ2137/vue_demo/src/components/day01/test2.vue
+++
@@ -1,6 +0,0 @@
-<!--
- * @Date: 2021-10-15 16:17:32
- * @Author: 浩
- * @LastEditors: 浩
- * @FilePath: \vue_demo\src\components\day01\test2.vue
--->
